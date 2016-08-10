adf744a38ca2b9c150b83375ea689d7cbbbb3d64
Freddie <freddie_yang@139.com>
2016-08-11 00:05:14 +0800

    add ble initial functions.

--- examples/ble/source/main.c
+++ examples/ble/source/main.c
@@ -36,60 +36,40 @@
 #include "app_error.h"
 
 
-#define TASK_DELAY        200    /**< Task delay. Delays a LED0 task for 200 ms */
-#define TIMER_PERIOD      1000   /**< Timer period. LED1 timer will expire after 1000 ms */
+#define SYSTEM_INDICATOR_LED_INTERVAL     5000     /**< Sensor Contact Detected toggle interval (ms). */
+#define APP_TIMER_PRESCALER                  0     /**< Value of the RTC1 PRESCALER register. */
+#define APP_TIMER_OP_QUEUE_SIZE           0x04     /**< Size of timer operation queues. */
+#define OSTIMER_WAIT_FOR_QUEUE               2     /**< Number of ticks to wait for the timer queue to be ready */
 
-/**@brief LED0 task entry function.
- *
- * @param[in] pvParameter   Pointer that will be used as the parameter for the task.
- */
-static void vLed0Function (void *pvParameter)
-{
-    UNUSED_PARAMETER(pvParameter);
-    for( ;; )
-    {
-        nrf_gpio_pin_toggle(BSP_LED_0);
-        vTaskDelay(TASK_DELAY); // Delay a task for a given number of ticks
+static TimerHandle_t m_system_indicator_timer;     /**< Definition of system indicator led timer. */
 
-        // Tasks must be implemented to never return...
-    }
-}
+static void clock_initialization();
+static void ble_stack_thread(void * arg);
+static void timers_init(void);
+static void system_indicator_timeout_handler(void * p_context);
+static void buttons_leds_init(bool * p_erase_bonds);
+static void ble_stack_init(void);
+static void device_manager_init(bool erase_bonds);
+static uint32_t device_manager_evt_handler(dm_handle_t const * p_handle,
+                                           dm_event_t const  * p_event,
+                                           ret_code_t        event_result);
+static void gap_params_init(void);
+static void advertising_init(void);
+static void advertising_start(void);
+static void services_init(void);
+static void conn_params_init(void);
+static void application_timers_start(void);
 
-/**@brief The function to call when the LED1 FreeRTOS timer expires.
- *
- * @param[in] pvParameter   Pointer that will be used as the parameter for the timer.
- */
-static void vLed1Callback (void *pvParameter)
-{
-    UNUSED_PARAMETER(pvParameter);
-    nrf_gpio_pin_toggle(BSP_LED_1);
-}
 
 int main(void)
 {
-    TaskHandle_t  xLed0Handle;       /**< Reference to LED0 toggling FreeRTOS task. */
-    TimerHandle_t xLed1Handle;       /**< Reference to LED1 toggling FreeRTOS timer. */
-    ret_code_t err_code;
-
-    err_code = nrf_drv_clock_init();
-    APP_ERROR_CHECK(err_code);
-
-    // Configure LED-pins as outputs
-    nrf_gpio_cfg_output(BSP_LED_0);
-    nrf_gpio_cfg_output(BSP_LED_1);
-    nrf_gpio_cfg_output(BSP_LED_2);
-    nrf_gpio_cfg_output(BSP_LED_3);
-	nrf_gpio_cfg_output(BSP_LED_4);
-    nrf_gpio_pin_clear(BSP_LED_0);
-    nrf_gpio_pin_clear(BSP_LED_1);
-    nrf_gpio_pin_clear(BSP_LED_2);
-    nrf_gpio_pin_clear(BSP_LED_3);
-	nrf_gpio_pin_clear(BSP_LED_4);
-
-    UNUSED_VARIABLE(xTaskCreate( vLed0Function, "L0", configMINIMAL_STACK_SIZE + 200, NULL, 2, &xLed0Handle ));    // LED0 task creation
-    xLed1Handle = xTimerCreate( "L1", TIMER_PERIOD, pdTRUE, NULL, vLed1Callback );                                 // LED1 timer creation
-    UNUSED_VARIABLE(xTimerStart( xLed1Handle, 0 ));                                                                // LED1 timer start
-
+    clock_initialization();
+    // Start execution.
+    if(pdPASS != xTaskCreate(ble_stack_thread, "BLE", 256, NULL, 1, &m_ble_stack_thread))
+    {
+        APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
+    }
+    
     /* Activate deep sleep mode */
     SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
 
@@ -99,6 +79,224 @@ int main(void)
     while (true)
     {
         // FreeRTOS should not be here...
+        APP_ERROR_HANDLER(NRF_ERROR_FORBIDDEN);
+    }
+}
+
+/**@brief Function for initialization oscillators.
+ */
+static void clock_initialization()
+{
+    /* Start 16 MHz crystal oscillator */
+    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+    NRF_CLOCK->TASKS_HFCLKSTART    = 1;
+
+    /* Wait for the external oscillator to start up */
+    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0)
+    {
+        // Do nothing.
+    }
+
+    /* Start low frequency crystal oscillator for app_timer(used by bsp)*/
+    NRF_CLOCK->LFCLKSRC            = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
+    NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
+    NRF_CLOCK->TASKS_LFCLKSTART    = 1;
+
+    while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0)
+    {
+        // Do nothing.
+    }
+}
+
+/**@brief Thread for handling the Application's BLE Stack events.
+ *
+ * @details This thread is responsible for handling BLE Stack events sent from on_ble_evt().
+ *
+ * @param[in]   arg   Pointer used for passing some arbitrary information (context) from the
+ *                    osThreadCreate() call to the thread.
+ */
+static void ble_stack_thread(void * arg)
+{
+    bool      erase_bonds;
+
+    UNUSED_PARAMETER(arg);
+
+    // Initialize.
+    timers_init();
+    buttons_leds_init(&erase_bonds);
+    ble_stack_init();
+    device_manager_init(erase_bonds);
+    gap_params_init();
+    advertising_init();
+    services_init();
+    conn_params_init();
+
+    application_timers_start();
+    advertising_start();
+
+    while (1)
+    {
+        
+    }
+}
+
+/**@brief Function for the Timer initialization.
+ *
+ * @details Initializes the timer module. This creates and starts application timers.
+ */
+static void timers_init(void)
+{
+    // Initialize timer module.
+    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, false);
+
+    // Create timers.
+    m_system_indicator_timer = xTimerCreate("LED", SYSTEM_INDICATOR_LED_INTERVAL, pdTRUE, NULL, system_indicator_timeout_handler);
+
+    /* Error checking */
+    if( NULL == m_system_indicator_timer )
+    {
+        APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
+    }
+}
+
+/**@brief Function for initializing buttons and leds.
+ *
+ * @param[out] p_erase_bonds  Will be true if the clear bonding button was pressed to wake the application up.
+ */
+static void buttons_leds_init(bool * p_erase_bonds)
+{
+    bsp_event_t startup_event;
+
+    uint32_t err_code = bsp_init(BSP_INIT_LED | BSP_INIT_BUTTONS,
+                                 APP_TIMER_TICKS(100, APP_TIMER_PRESCALER),
+                                 bsp_event_handler);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = bsp_btn_ble_init(NULL, &startup_event);
+    APP_ERROR_CHECK(err_code);
+
+    *p_erase_bonds = (startup_event == BSP_EVENT_CLEAR_BONDING_DATA);
+}
+
+/**@brief Function for handling the system indicator timer timeout.
+ *
+ * @details This function will be called each time the  timer expires.
+ *
+ * @param[in] p_context   Pointer used for passing some arbitrary information (context) from the
+ *                        app_start_timer() call to the timeout handler.
+ */
+static void system_indicator_timeout_handler(void * p_context)
+{
+    UNUSED_PARAMETER(p_context);
+}
+
+/**@brief Function for initializing the BLE stack.
+ *
+ * @details Initializes the SoftDevice and the BLE event interrupt.
+ */
+static void ble_stack_init(void)
+{
+    NRF_RNG->TASKS_START = 1;
+}
+
+/**@brief Function for the Device Manager initialization.
+ *
+ * @param[in] erase_bonds  Indicates whether bonding information should be cleared from
+ *                         persistent storage during initialization of the Device Manager.
+ */
+static void device_manager_init(bool erase_bonds)
+{
+    uint32_t               err_code;
+    dm_init_param_t        init_param = {.clear_persistent_data = erase_bonds};
+    dm_application_param_t register_param;
+
+    // Initialize persistent storage module.
+    err_code = pstorage_init();
+    APP_ERROR_CHECK(err_code);
+
+    err_code = dm_init(&init_param);
+    APP_ERROR_CHECK(err_code);
+
+    memset(&register_param.sec_param, 0, sizeof(ble_gap_sec_params_t));
+
+    register_param.sec_param.bond         = SEC_PARAM_BOND;
+    register_param.sec_param.mitm         = SEC_PARAM_MITM;
+    register_param.sec_param.lesc         = SEC_PARAM_LESC;
+    register_param.sec_param.keypress     = SEC_PARAM_KEYPRESS;
+    register_param.sec_param.io_caps      = SEC_PARAM_IO_CAPABILITIES;
+    register_param.sec_param.oob          = SEC_PARAM_OOB;
+    register_param.sec_param.min_key_size = SEC_PARAM_MIN_KEY_SIZE;
+    register_param.sec_param.max_key_size = SEC_PARAM_MAX_KEY_SIZE;
+    register_param.evt_handler            = device_manager_evt_handler;
+    register_param.service_type           = DM_PROTOCOL_CNTXT_GATT_SRVR_ID;
+
+    err_code = dm_register(&m_app_handle, &register_param);
+    APP_ERROR_CHECK(err_code);
+}
+
+/**@brief Function for handling the Device Manager events.
+ *
+ * @param[in]   p_evt   Data associated to the device manager event.
+ */
+static uint32_t device_manager_evt_handler(dm_handle_t const * p_handle,
+                                           dm_event_t const  * p_event,
+                                           ret_code_t        event_result)
+{
+    APP_ERROR_CHECK(event_result);
+    return NRF_SUCCESS;
+}
+
+/**@brief Function for the GAP initialization.
+ *
+ * @details This function sets up all the necessary GAP (Generic Access Profile) parameters of the
+ *          device including the device name, appearance, and the preferred connection parameters.
+ */
+static void gap_params_init(void)
+{
+    
+}
+
+/**@brief Function for initializing the Advertising functionality.
+ */
+static void advertising_init(void)
+{
+    
+}
+
+/**@brief Function for starting advertising.
+ */
+static void advertising_start(void)
+{
+    uint32_t err_code;
+
+    err_code = bsp_indication_set(BSP_INDICATE_ADVERTISING);
+    APP_ERROR_CHECK(err_code);
+}
+
+/**@brief Function for initializing services that will be used by the application.
+ *
+ * @details Initialize the Heart Rate, Battery and Device Information services.
+ */
+static void services_init(void)
+{
+    
+}
+
+/**@brief Function for initializing the Connection Parameters module.
+ */
+static void conn_params_init(void)
+{
+    
+}
+
+/**@brief Function for starting application timers.
+ */
+static void application_timers_start(void)
+{
+    // Start application timers.
+    if(pdPASS != xTimerStart(m_system_indicator_timer, OSTIMER_WAIT_FOR_QUEUE))
+    {
+        APP_ERROR_HANDLER(NRF_ERROR_NO_MEM);
     }
 }
 
