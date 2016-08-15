0d8ba95eaf2bf86b79af07cbcdd070dc594bdfbb
yangyufeng <yangyufeng@xiaomi.com>
2016-08-15 21:41:52 +0800

    change position of some header files.

--- examples/ble/include/main.h
+++ examples/ble/include/main.h
@@ -1,6 +1,20 @@
 #ifndef MAIN_H__
 #define MAIN_H__
 
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "bsp.h"
+#include "nordic_common.h"
+#include "nrf_gpio.h"
+#include "nrf_error.h"
+#include "nrf_drv_clock.h"
+#include "sdk_errors.h"
+#include "app_error.h"
+
 #define APP_TASK_BLE_STACK_SIZE            256     /**< Size of stack for BLE task. */
 #define APP_TASK_BLE_PRIORITY                1     /**< priority of BLE task. */
 
--- examples/ble/source/main.c
+++ examples/ble/source/main.c
@@ -25,19 +25,7 @@
  * INCLUDES
  */
 
-#include <stdbool.h>
-#include <stdint.h>
-
-#include "FreeRTOS.h"
-#include "task.h"
-#include "timers.h"
-#include "bsp.h"
-#include "nordic_common.h"
-#include "nrf_gpio.h"
-#include "nrf_error.h"
-#include "nrf_drv_clock.h"
-#include "sdk_errors.h"
-#include "app_error.h"
+#include "main.h"
 
 /*********************************************************************
  * CONSTANTS
